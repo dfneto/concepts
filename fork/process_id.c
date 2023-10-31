07ab0ec60ba9a77e3a170e472aa26925f7ee8ac8
davifern <pdavidfneto@gmail.com>
2023-10-31 20:40:17 +0100

    understanding fork and ids

---
+++ fork/process_id.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+int	main()
+{
+	printf("My id before fork: %d\n", getpid());
+	printf("----------------------------\n");
+	int id = fork();
+	int	res = wait(NULL);
+	if (res != -1) //processo pai
+	{
+		printf("The child process %d finished your execution\n", res);
+		printf("The parent process received from the fork the id: %d (that is from your son)\n", id); //o fork retorna para o processo pai o id do seu filho e 0 para o filho
+		printf("Current ID: %d, parent ID: %d\n", getpid(), getppid());
+	}
+	else //processo filho
+	{
+		printf("No children to wait for, probably because I'm the child\n");
+		printf("The child process received from the fork the id: %d\n", id); //0
+		printf("Current ID: %d, parent ID: %d\n", getpid(), getppid());
+		printf("----------------------------\n");
+	}
+
+	return (0);
+}
---
+++ fork/starting.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+/*
+ * When performing input or output operations, C often uses buffering. Instead of writing data directly to the destination (like a file or the terminal) every time a write is requested, the data is temporarily stored in a buffer. After the buffer is filled, or under certain conditions, the data is then flushed to its destination. This buffering mechanism enhances performance by reducing the number of direct write or read operations.
+
+* The fflush() function is used to force the buffer to be written (flushed) to the destination before the buffer is full or before a normal flush would occur.
+*/
+/*
+ *the **`wait()`** function is a system call used to make the parent process wait until all of its child processes have terminated.
+
+- If successful, **`wait()`** returns the Process ID (PID) of the terminated child.
+- If the calling process has no child processes, **`wait()`** returns **`1`**.
+Com wait basicamente vc tem primeiro o filho sendo executado e depois o pai.
+ */
+int	main()
+{
+	int	id = fork();
+	int	n;
+	int i;
+
+	if (id == 0) //sou o processo filho
+		n = 1;
+	else
+		n = 6;
+	if (id != 0)
+		wait(0);
+	for (i = n; i < n + 5; i++)
+	{
+		printf("%d ", i);
+		fflush(stdout);
+	}
+	if (id != 0)
+		printf("\n");
+	return (0);
+}
